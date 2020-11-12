20023ccd22ca5c6e519ede76afc6658528a27dea
Rustin Mirzaei <rustinmirzaei@gmail.com>
2020-11-12 15:13:34 +0330

    models

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Post
 
 
 def home(request):
-    return render(request, "blog/home.html")
+    posts = Post.objects.all()
+    return render(request, "blog/home.html", {"posts": posts})
