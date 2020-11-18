fdfc1e80bcf6acd6baccaf1345a989453eee933b
Rustin Mirzaei <rustinmirzaei@gmail.com>
2020-11-18 10:26:10 +0330

    using bootstrap blog example as temaple

--- website/blog/urls.py
+++ website/blog/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "blog"
 urlpatterns = [
     path("", views.home, name="home"),
+    path("about/", views.about, name="about"),
 ]
--- website/blog/views.py
+++ website/blog/views.py
@@ -6,3 +6,7 @@ from .models import Post
 def home(request):
     posts = Post.objects.all()
     return render(request, "blog/home.html", {"posts": posts})
+
+
+def about(request):
+    return render(request, "blog/about.html")
