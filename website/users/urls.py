662e77f24c6a234330b6eecbbdf09cc9097e1c97
Rustin Mirzaei <rustinmirzaei@gmail.com>
2020-11-25 15:31:13 +0330

    change template to be simaple

--- website/users/urls.py
+++ website/users/urls.py
@@ -13,9 +13,6 @@ urlpatterns = [
         auth_view.LoginView.as_view(template_name="users/login.html"),
         name="login",
     ),
-    path(
-        "logout/",
-        auth_view.LogoutView.as_view(),
-        name="logout",
-    ),
+    # path("logout/", auth_view.LogoutView.as_view(),name="logout",),
+    path("logout/", views.logout_view, name='logout'),
 ]
--- website/users/views.py
+++ website/users/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth import views as auth_view
+from django.utils.safestring import mark_safe
+from django.contrib.auth.decorators import login_required
 
 from .forms import UserRegisterationForm
 
@@ -17,6 +22,15 @@ def register(request):
 
     return render(request, "users/register.html", {"form": form})
 
-
+@login_required
 def profile(request):
     return render(request, "users/profile.html")
+
+def logout_view(request):
+    login_url = reverse('users:login')
+    msg = mark_safe(f"""Logut success. <a href="{login_url}">Login</a> again""")
+    messages.success(request, f"{msg}")
+    logout(request)
+    return redirect("blog:home")
+
+
